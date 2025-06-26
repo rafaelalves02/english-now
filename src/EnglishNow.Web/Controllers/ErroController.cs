0f4bb37f97d370e6652aa0fcf7958bd459523e0b
Rafael Alves De Carvalho <rafaelalvesdecarvalho25@gmail.com>
2025-06-26 16:15:51 -0300

    fazendo tela de erro

---
+++ src/EnglishNow.Web/Controllers/ErroController.cs
@@ -0,0 +1,19 @@
+﻿using EnglishNow.Web.Models.Erro;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishNow.Web.Controllers
+{
+    public class ErroController : Controller
+    {
+        public IActionResult Index()
+        {
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var model = new ErroViewModel
+            {
+                MensagemErro = exceptionHandlerFeature == null ? "erro inesperado" : exceptionHandlerFeature.Error.Message
+            };
+            return View();
+        }
+    }
+}
