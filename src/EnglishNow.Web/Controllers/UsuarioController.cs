03438723128772ff38eb604262e558694168cf2b
Rafael Alves De Carvalho <rafaelalvesdecarvalho25@gmail.com>
2025-07-03 18:11:02 -0300

    Criando tela de login

---
+++ src/EnglishNow.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EnglishNow.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
